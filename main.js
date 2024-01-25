6daa17ee209960e5cd7d902e59803d38d102de2c
chaitanyasphoorthi <88782032+chaitanyasphoorthi@users.noreply.github.com>
2024-01-25 19:42:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function Page1(){
+    window.location = "bedroom.html"
+}
+function Page2(){
+    window.location = "kitchen.html"
+}
+function Page3(){
+    window.location = "desk.html"
+}
+function Page4(){
+    window.location = "livingroom.html"
+}
+
